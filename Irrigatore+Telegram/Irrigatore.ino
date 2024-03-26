b6a1e41b7cc54233398e5fe01fbe9b8f990ff7b2
Pasquale <61435712+Psq333@users.noreply.github.com>
2024-03-26 19:24:02 +0100

    Add files via upload

---
+++ Irrigatore+Telegram/Irrigatore.ino
@@ -0,0 +1,124 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/telegram-control-esp32-esp8266-nodemcu-outputs/
+  
+  Project created using Brian Lough's Universal Telegram Bot Library: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
+  Example based on the Universal Arduino Telegram Bot Library: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot/blob/master/examples/ESP8266/FlashLED/FlashLED.ino
+*/
+
+#ifdef ESP32
+  #include <WiFi.h>
+#else
+  #include <ESP8266WiFi.h>
+#endif
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>   // Universal Telegram Bot Library written by Brian Lough: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
+#include <ArduinoJson.h>
+
+// Replace with your network credentials
+const char* ssid = "FASTWEB-HA9REQ";
+const char* password = "7EEDFJN6XG";
+
+// Initialize Telegram BOT
+#define BOTtoken "5782449586:AAGK0fNrKZxeh7N2bB4VBLXNxVljMWnugKE"  // your Bot Token (Get from Botfather)
+
+// Use @myidbot to find out the chat ID of an individual or a group
+// Also note that you need to click "start" on a bot before it can
+// message you
+#define CHAT_ID "659291299"
+
+#ifdef ESP8266
+  X509List cert(TELEGRAM_CERTIFICATE_ROOT);
+#endif
+
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
+
+// Checks for new messages every 1 second.
+int botRequestDelay = 1000;
+unsigned long lastTimeBotRan;
+
+const int irrPin = 2;
+bool irrState = LOW;
+
+// Handle what happens when you receive new messages
+void handleNewMessages(int numNewMessages) {
+  Serial.println("handleNewMessages");
+  Serial.println(String(numNewMessages));
+
+  for (int i=0; i<numNewMessages; i++) {
+    // Chat id of the requester
+    String chat_id = String(bot.messages[i].chat_id);
+    if (chat_id != CHAT_ID){
+      bot.sendMessage(chat_id, "Unauthorized user", "");
+      continue;
+    }
+    
+    // Print the received message
+    String text = bot.messages[i].text;
+    Serial.println("\n La parola è: " + text + "\n\n");
+
+    String from_name = bot.messages[i].from_name;
+
+    if (text == "/start") {
+      String welcome = "Welcome, " + from_name + ".\n";
+      welcome += "Use the following commands to control your outputs.\n\n";
+      welcome += "/irrigatore_on to turn GPIO ON \n";
+      bot.sendMessage(chat_id, welcome, "");
+    }
+
+    if (text == "/irrigatore_on") {
+      bot.sendMessage(chat_id, "Irrigator state set to ON", "");
+      irrState = HIGH;
+      digitalWrite(irrPin, irrState);
+      bot.sendMessage(chat_id, "Irrigator works for 20 seconds", "");
+      delay(20000);
+      irrState = LOW;
+      bot.sendMessage(chat_id, "Irrigator state set to OFF", "");
+      digitalWrite(irrPin, irrState);
+    }
+    
+  }
+}
+
+void setup() {
+  #ifdef ESP8266
+  client.setInsecure();
+#endif
+  Serial.begin(115200);
+
+  #ifdef ESP8266
+    configTime(0, 0, "pool.ntp.org");      // get UTC time via NTP
+    client.setTrustAnchors(&cert); // Add root certificate for api.telegram.org
+  #endif
+
+  pinMode(irrPin, OUTPUT);
+  digitalWrite(irrPin, irrState);
+  
+  // Connect to Wi-Fi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  #ifdef ESP32
+    client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
+  #endif
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+  // Print ESP32 Local IP Address
+  Serial.println(WiFi.localIP());
+  bot.sendMessage(CHAT_ID, "Irrigator is connect to Wi-Fi", "");
+}
+
+void loop() {
+  if (millis() > lastTimeBotRan + botRequestDelay)  {
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+
+    while(numNewMessages) {
+      Serial.println("got response");
+      handleNewMessages(numNewMessages);
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    }
+    lastTimeBotRan = millis();
+  }
+}
